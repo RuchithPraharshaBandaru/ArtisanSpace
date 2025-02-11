ff851ceb84b65870d99f3300ddf312fe0f374f10
Rithvik086 <rithvikrao2005@gmail.com>
2025-02-11 22:55:45 +0530

    auth implemented

---
+++ src/app.js
@@ -0,0 +1,42 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import authroutes from "./routes/authroutes.js"
+import useroutes from './routes/userRoutes.js'
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+
+dotenv.config({ path: path.resolve(__dirname, '../.env') });
+
+import dbConnect from './config/dbconnect.js';
+
+dbConnect();
+
+const app = express();
+const port = 3000;
+const hostname = '127.0.0.1';
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.set('view engine', 'ejs');
+
+// app.get('/auth', (req, res) => {
+//   res.render('auth');
+// });
+
+app.use('/auth',authroutes)
+app.use('/users',useroutes)
+
+// Starts an Express server locally on port 3000
+app.listen(port, hostname, () => {
+  console.log(`Listening on http://${hostname}:${port}/`);
+});
---
+++ src/models/usermodel.js
@@ -0,0 +1,36 @@
+import mongoose, { mongo, MongooseError } from "mongoose";
+
+const userSchema = new mongoose.Schema({
+username:{
+    type: String,
+    required : true,
+    unique: true
+}, 
+
+email:{
+    type : String,
+    required : true,
+    unique : true
+
+},
+
+password:{
+    type:String,
+    required : true,
+},
+
+role : {
+    type : String,
+    required : true,
+enum : ["admin","artisan","customer"]
+}
+
+},
+{
+    timestamps : true
+}
+)
+
+
+
+export default mongoose.model("User", userSchema);
