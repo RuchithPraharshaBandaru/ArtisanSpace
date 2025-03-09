ac1f1ee60218fb6540ae9029c0918704b9591c76
Excius <rao.daksh555@gmail.com>
2025-03-10 04:17:13 +0530

    improved store

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,9 +1,8 @@
 import express from "express";
 import verifytoken from "../middleware/authMiddleware.js";
 
-import authorizeroles from '../middleware/roleMiddleware.js'
-import customerroles from '../routes/customerroutes.js'
-
+import authorizeroles from "../middleware/roleMiddleware.js";
+import customerroles from "../routes/customerroutes.js";
 
 const router = express.Router();
 
@@ -13,8 +12,7 @@ router.use(verifytoken);
 //     res.render("store", { products });
 // });
 
-
-router.use('/customer', customerroles)
+router.use("/customer", customerroles);
 
 router.get("/admin", authorizeroles("admin"), (req, res) => {
   res.json({ message: "Welcome Admin" });
@@ -24,11 +22,9 @@ router.get("/manager", authorizeroles("admin", "manager"), (req, res) => {
   res.json({ message: "Welcome Manager" });
 });
 
-
 router.get("/manager", authorizeroles("admin", "manager"), (req, res) => {
   res.json({ message: "Welcome Manager" });
 });
-
 
 router.get(
   "/artisan",
@@ -38,10 +34,12 @@ router.get(
   },
 );
 
-
-router.get("/customer", authorizeroles("admin", "manager", "customer"), (req, res) => {
-  res.json({ message: "Welcome Customer" });
-},
+router.get(
+  "/customer",
+  authorizeroles("admin", "manager", "customer"),
+  (req, res) => {
+    res.json({ message: "Welcome Customer" });
+  },
 );
 
 export default router;
