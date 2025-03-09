658c5ed213a5e4fe3299afecfdeb0ba10c0f2aa9
Excius <rao.daksh555@gmail.com>
2025-03-09 18:58:45 +0530

    Added store page and few tweeks

--- src/controller/authcontroller.js
+++ src/controller/authcontroller.js
@@ -1,57 +1,51 @@
-import User from "../models/usermodel.js"
-import bcrypt from 'bcryptjs'
-import jwt from 'jsonwebtoken'
-
+import User from "../models/usermodel.js";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 const signup = async (req, res) => {
-    const { username, email, password, role } = req.body;
-    //console.log('body',username,password,email,role);
-    //salt is the team number
-    const hashpass = await bcrypt.hash(password, 9);
+  const { username, email, password, role } = req.body;
+  //console.log('body',username,password,email,role);
+  //salt is the team number
+  const hashpass = await bcrypt.hash(password, 9);
 
-    const newUser = new User({ username, email, password: hashpass, role })
-    await newUser.save()
-  
+  const newUser = new User({ username, email, password: hashpass, role });
+  await newUser.save();
 
-    res.redirect("/login");
-
-}
-
+  res.redirect("/login");
+};
 
 const login = async (req, res) => {
+  const { username, password } = req.body;
 
-    const { username, password } = req.body
-   
+  const user = await User.findOne({ username });
+  if (!user) {
+    return res.status(404).json({ message: "user not found" });
+  }
 
-    const user = await User.findOne({username})
-    if(!user){
-        return res.status(404).json({message : "user not found"})
-    }
+  const ismatched = await bcrypt.compare(password, user.password);
 
-    const ismatched =  await bcrypt.compare(password, user.password)
+  if (!ismatched) {
+    return res.status(400).json({ message: "invalid credentials" });
+  }
 
-    if(!ismatched){
-        return res.status(400).json({message : 'invalid credentials'})
-    }
+  const token = jwt.sign(
+    {
+      id: user._id,
+      role: user.role,
+    },
+    process.env.JWT_SECRET,
+    { expiresIn: "24h" },
+  );
 
-    const token = jwt.sign({
-        id:user._id , role: user.role
-    }, process.env.JWT_SECRET, {expiresIn: "1h"});
+  res.cookie("token", token, {
+    //modify to use https while in production during deployment
+    httpOnly: true,
+    sameSite: "Strict",
+    maxAge: 86400000,
+  });
 
+  // res.status(200).json({ message: "Login successful" });
+  res.redirect("/");
+};
 
-
-
-
-    res.cookie("token",token,{
-        //modify to use https while in production during deployment
-        httpOnly : true,
-        sameSite: "Strict",
-        maxAge: 3600000,
-    });
-
-    res.status(200).json({message: "Login successful" })
-
-}
-
-
-export { signup, login }
+export { signup, login };
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,23 +1,88 @@
 import express from 'express';
 import verifytoken from '../middleware/authMiddleware.js';
 
-import  authorizeroles from '../middleware/roleMiddleware.js'
+import authorizeroles from '../middleware/roleMiddleware.js'
+
+const products = [
+    {
+        name: "Brass Buddha Door Knocker",
+        image: "/images/buddha-knocker.jpg",
+        oldPrice: "₹1,999.00",
+        newPrice: "₹1,499.00",
+        sale: true,
+    },
+    {
+        name: "Brass Dancing Lord Ganesha Idol",
+        image: "/images/ganesha-idol1.jpg",
+        oldPrice: "₹3,999.00",
+        newPrice: "₹2,999.00",
+        sale: true,
+    },
+    {
+        name: "Brass Dancing Lord Ganesha Idol",
+        image: "/images/ganesha-idol2.jpg",
+        oldPrice: "₹999.00",
+        newPrice: "₹499.00",
+        sale: true,
+    },
+    {
+        name: "Brass Dancing Lord Ganesha Idol",
+        image: "/images/ganesha-idol3.jpg",
+        oldPrice: "₹2,999.00",
+        newPrice: "₹1,999.00",
+        sale: true,
+    },
+    {
+        name: "Brass Buddha Door Knocker",
+        image: "/images/buddha-knocker.jpg",
+        oldPrice: "₹1,999.00",
+        newPrice: "₹1,499.00",
+        sale: true,
+    },
+    {
+        name: "Brass Dancing Lord Ganesha Idol",
+        image: "/images/ganesha-idol1.jpg",
+        oldPrice: "₹3,999.00",
+        newPrice: "₹2,999.00",
+        sale: true,
+    },
+    {
+        name: "Brass Dancing Lord Ganesha Idol",
+        image: "/images/ganesha-idol2.jpg",
+        oldPrice: "₹999.00",
+        newPrice: "₹499.00",
+        sale: true,
+    },
+    {
+        name: "Brass Dancing Lord Ganesha Idol",
+        image: "/images/ganesha-idol3.jpg",
+        oldPrice: "₹2,999.00",
+        newPrice: "₹1,999.00",
+        sale: true,
+    },
+];
 
 const router = express.Router();
 
-router.get("/admin", verifytoken, authorizeroles("admin"),(req, res) => {
+router.use(verifytoken);
+
+router.get("/store", (req, res) => {
+    res.render("store", { products });
+});
+
+router.get("/admin", authorizeroles("admin"), (req, res) => {
     res.json({ message: "Welcome Admin" });
 });
 
-router.get("/artisan", verifytoken, authorizeroles("admin","manager","artisan"),(req, res) => {
+router.get("/artisan", authorizeroles("admin", "manager", "artisan"), (req, res) => {
     res.json({ message: "Welcome Artisan" });
 });
 
-router.get("/manager", verifytoken,authorizeroles("admin","manager"), (req, res) => {
+router.get("/manager", authorizeroles("admin", "manager"), (req, res) => {
     res.json({ message: "Welcome Manager" });
 });
 
-router.get("/customer", verifytoken, authorizeroles("admin","manager","user"),(req, res) => {
+router.get("/customer", authorizeroles("admin", "manager", "user"), (req, res) => {
     res.json({ message: "Welcome Customer" });
 });
 
