188e62a02b2912f855b787343ef926e2ce9570fc
Excius <rao.daksh555@gmail.com>
2025-03-12 02:28:59 +0530

    added card add and del functionality with cartDB with functions

---
+++ src/models/cartmodel.js
@@ -0,0 +1,92 @@
+import fs from "fs/promises";
+import { fileURLToPath } from "url";
+import path from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const cartPath = path.join(__dirname, "../../carts.json");
+
+async function ensureFileExists(filePath) {
+  try {
+    await fs.access(filePath);
+  } catch {
+    await fs.writeFile(filePath, "[]");
+  }
+}
+
+async function readData(filePath) {
+  await ensureFileExists(filePath);
+  const data = await fs.readFile(filePath, "utf8");
+  return JSON.parse(data);
+}
+
+async function writeData(data, filePath) {
+  await fs.writeFile(filePath, JSON.stringify(data, null, 2));
+}
+
+export async function getCart(userId) {
+  userId = parseInt(userId);
+  const carts = await readData(cartPath);
+
+  let userCart = carts.find((cart) => cart.userId === userId);
+  if (userCart) {
+    return userCart;
+  } else {
+    return { userId, cart: [] };
+  }
+}
+
+export async function addItem(userId, productId) {
+  userId = parseInt(userId);
+  productId = parseInt(productId);
+  const carts = await readData(cartPath);
+
+  let userCart = carts.find((cart) => cart.userId === userId);
+
+  if (userCart) {
+    let product = userCart.cart.find((item) => item.productId === productId);
+
+    if (product) {
+      product.quantity += 1;
+    } else {
+      userCart.cart.push({ productId, quantity: 1 });
+    }
+  } else {
+    carts.push({ userId, cart: [{ productId, quantity: 1 }] });
+  }
+
+  await writeData(carts, cartPath);
+  console.log("Product added to cart");
+}
+
+export async function deleteItem(userId, productId) {
+  userId = parseInt(userId);
+  productId = parseInt(productId);
+  const carts = await readData(cartPath);
+
+  let userCart = carts.find((cart) => cart.userId === userId);
+
+  if (userCart) {
+    let productIndex = userCart.cart.findIndex(
+      (item) => item.productId === productId
+    );
+
+    if (productIndex !== -1) {
+      let product = userCart.cart[productIndex];
+
+      if (product.quantity > 1) {
+        product.quantity -= 1;
+      } else {
+        userCart.cart.splice(productIndex, 1);
+      }
+
+      if (userCart.cart.length === 0) {
+        let userIndex = carts.findIndex((cart) => cart.userId === userId);
+        carts.splice(userIndex, 1);
+      }
+    }
+  }
+  await writeData(carts, cartPath);
+  console.log("Product deleted from cart");
+}
--- src/routes/customerroutes.js
+++ src/routes/customerroutes.js
@@ -1,12 +1,15 @@
 import express from "express";
 import { getProducts } from "../models/productmodel.js";
+import { addItem, deleteItem, getCart } from "../models/cartmodel.js";
 
 const router = express.Router();
 
 const custrole = "customer";
 
-router.get("/", (req, res) => {
-  res.render("customer/customerhome", { role: custrole });
+router.get("/", async (req, res) => {
+  const products = await getProducts();
+  const pro = products.slice(0, 10);
+  res.render("customer/customerhome", { role: custrole, products: pro });
 });
 router.get("/workshop", (req, res) => {
   res.render("customer/workshop", { role: custrole });
@@ -15,9 +18,38 @@ router.get("/settings", (req, res) => {
   res.json({ message: "settings" });
 });
 
-router.get("/orders", (req, res) => {
-  res.render("customer/customerorders", { role: custrole });
+router.get("/orders", async (req, res) => {
+  const userId = req.user.id;
+  const products = await getProducts();
+  let cart = await getCart(userId);
+  if (req.headers["x-requested-with"] === "XMLHttpRequest") {
+    res.render("partials/cart", { cart, products, userId });
+  } else {
+    res.render("customer/customerorders", {
+      role: custrole,
+      cart,
+      products,
+      userId,
+    });
+  }
 });
+
+router.post("/orders", async (req, res) => {
+  try {
+    const { userId, productId, action } = req.query;
+    if (action === "add") {
+      await addItem(userId, productId);
+    } else if (action === "del") {
+      await deleteItem(userId, productId);
+    }
+
+    res.json({ message: "Cart updated successfully" });
+  } catch (error) {
+    console.error("Error processing request:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
 router.get("/contactus", (req, res) => {
   res.render("customer/customercontactus", { role: custrole });
 });
