7bb7433ce9b131075077e88812077b5203b3473b
Excius <rao.daksh555@gmail.com>
2025-03-12 21:51:42 +0530

    added full backend functionality for order page

--- src/routes/customerroutes.js
+++ src/routes/customerroutes.js
@@ -1,6 +1,11 @@
 import express from "express";
 import { getProducts } from "../models/productmodel.js";
-import { addItem, deleteItem, getCart } from "../models/cartmodel.js";
+import {
+  addItem,
+  deleteItem,
+  getCart,
+  removeCompleteItem,
+} from "../models/cartmodel.js";
 
 const router = express.Router();
 
@@ -21,15 +26,23 @@ router.get("/settings", (req, res) => {
 router.get("/orders", async (req, res) => {
   const userId = req.user.id;
   const products = await getProducts();
-  let cart = await getCart(userId);
+  const cart = await getCart(userId);
+
+  let amount = 0;
+  for (const item of cart.cart) {
+    const product = products.find((product) => product.id === item.productId);
+    amount += item.quantity * product.newPrice;
+  }
+
   if (req.headers["x-requested-with"] === "XMLHttpRequest") {
-    res.render("partials/cart", { cart, products, userId });
+    res.render("partials/cart", { cart, products, userId, amount });
   } else {
     res.render("customer/customerorders", {
       role: custrole,
       cart,
       products,
       userId,
+      amount,
     });
   }
 });
@@ -41,8 +54,9 @@ router.post("/orders", async (req, res) => {
       await addItem(userId, productId);
     } else if (action === "del") {
       await deleteItem(userId, productId);
+    } else if (action === "rem") {
+      await removeCompleteItem(userId, productId);
     }
-
     res.json({ message: "Cart updated successfully" });
   } catch (error) {
     console.error("Error processing request:", error);
@@ -59,6 +73,7 @@ router.get("/aboutus", (req, res) => {
 
 router.get("/store", async (req, res) => {
   try {
+    const userId = req.user.id;
     const products = await getProducts();
 
     const page = parseInt(req.query.page) || 1;
@@ -75,9 +90,21 @@ router.get("/store", async (req, res) => {
       currentPage: page,
       totalPages: Math.ceil(products.length / limit),
       role: custrole,
+      userId,
     });
   } catch (error) {
     res.status(500).send(error.message);
   }
 });
+
+router.post("/store", async (req, res) => {
+  const { userId, productId } = req.query;
+  try {
+    await addItem(userId, productId);
+  } catch (error) {
+    console.error("Error processing request:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
 export default router;
