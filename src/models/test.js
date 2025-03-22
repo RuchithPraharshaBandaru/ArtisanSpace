fa6ddaed035de4258d81258f5a99b45b530661cd
Excius <rao.daksh555@gmail.com>
2025-03-23 00:59:42 +0530

    fixed biggest bug, initializing database before running anything

--- src/models/test.js
+++ src/models/test.js
@@ -1,14 +1,30 @@
 // import module and test your functions inside the test function
 
-import { addRequest, approveRequest, deleteRequest, getRequests } from "./customordermodel.js";
+import { initializeDatabases } from "../config/sqlite.js";
+import {
+  acceptWorkshop,
+  bookWorkshop,
+  getAcceptedWorkshops,
+  getAvailableWorkshops,
+  getWorkshops,
+  removeWorkshop,
+} from "./workshopmodel.js";
 
+await initializeDatabases();
 
 async function test() {
-  // console.log(await addRequest("fed789d9-3fcc-40ed-9f44-b83b48692870","yeh second wala hundi","brass","xyz","test karle","122000","12:30:35"));
-  // console.log(await approveRequest("bae8c2c5-dcf4-4994-9777-8d67eb85ac5d","fed789d9-3fcc-40ed-9f44-b83b48692870"));
-  // console.log(await getRequests(true,"fed789d9-3fcc-40ed-9f44-b83b48692870" ));
-
-  // console.log(await deleteRequest("a255d65d-c333-4186-8256-858b6e81d094"));
-
+  // console.log(
+  //   await bookWorkshop(
+  //     "777b1dbc-58f3-41a4-9b7f-ccb4064d60e5",
+  //     "test title",
+  //     "test desc",
+  //     "today",
+  //     "idk"
+  //   )
+  // );
+  console.log(await getAvailableWorkshops());
+  // console.log(await getAcceptedWorkshops());
+  // console.log(await acceptWorkshop("05e3aa7c-be5e-4848-b096-b7cc3a88c999"));
+  // console.log(await removeWorkshop("938812c9-35aa-4b0c-98c8-acec3586bbad"));
 }
 await test();
