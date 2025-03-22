bff3791a653ad4ba24db933d5cf654f9628fd599
Rithvik086 <rithvikrao2005@gmail.com>
2025-03-23 01:37:55 +0530

    temp custom

--- src/models/test.js
+++ src/models/test.js
@@ -1,16 +1,14 @@
 // import module and test your functions inside the test function
 
-import { bookWorkshop } from "./workshopmodel.js";
+import { addRequest, approveRequest, deleteRequest, getRequests } from "./customordermodel.js";
+
 
 async function test() {
-  const result = await bookWorkshop(
-    "John Doe",
-    "john@example.com",
-    "1234567890",
-    "Pottery Workshop",
-    "Learn the basics of pottery making",
-    "2025-04-15",
-    "14:00-16:00"
-  );
+  // console.log(await addRequest("fed789d9-3fcc-40ed-9f44-b83b48692870","yeh second wala hundi","brass","xyz","test karle","122000","12:30:35"));
+  // console.log(await approveRequest("bae8c2c5-dcf4-4994-9777-8d67eb85ac5d","fed789d9-3fcc-40ed-9f44-b83b48692870"));
+  // console.log(await getRequests(true,"fed789d9-3fcc-40ed-9f44-b83b48692870" ));
+
+  // console.log(await deleteRequest("a255d65d-c333-4186-8256-858b6e81d094"));
+
 }
 test();
